5e2396da9a55c0785fd676d67bc642fa49d417b5
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-10-18 23:38:12 +0530

    Added reverse array program

---
+++ Array/reverse.cpp
@@ -0,0 +1,11 @@
+void reverseArray(vector<int> &arr , int m) {
+    // Write your code here  
+    int i = m+1,j = arr.size()-1;
+    while(i < j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
