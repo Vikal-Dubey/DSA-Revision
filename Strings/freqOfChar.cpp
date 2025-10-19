d38321f04fa040361cdb610c9a01a09e2b19c651
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-10-19 15:00:06 +0530

    Added <Count the freq of the characters>

---
+++ Strings/freqOfChar.cpp
@@ -0,0 +1,15 @@
+void Printfrequency(string str)
+{
+  int freq[26] = {0};
+  for (int i = 0; i < str.length(); i++)
+  {
+    freq[str[i] - 'a']++;
+  }
+  for (int i = 0; i < 26; i++)
+  {
+    if (freq[i] != 0)
+    {
+      cout << (char)(i + 'a') << freq[i] << " ";
+    }
+  }
+}
