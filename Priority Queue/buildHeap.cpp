d88d8d0f773407ac1f34d68cefde110f714cc4fc
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-10-19 23:21:49 +0530

    Added <Heap building by heapify>

---
+++ Priority Queue/buildHeap.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+vector<int> buildHeap(vector<int> arr, int n) {
+    int startIdx = (n / 2) - 1;
+
+    for (int i = startIdx; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    return arr;
+}
