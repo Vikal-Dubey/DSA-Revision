df837823e4a50f7b27e4b35c93f04c206db45847
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-11-09 17:16:56 +0530

    Added < 208. Implement Trie (Prefix Tree) >

---
+++ Trie/trie.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    Node* links[26];
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+    void setEnd() {
+        flag = true;
+    }
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(int i = 0; i < word.size(); i++) {
+            if(!node -> containsKey(word[i])) {
+                node -> put(word[i],new Node());
+            }
+            node = node -> get(word[i]);
+        }
+        node -> setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(int i = 0; i < word.size(); i++) {
+            if(!node -> containsKey(word[i])) {
+                return false;
+            }
+            node = node -> get(word[i]);
+        }
+        return node -> flag;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(int i = 0; i < prefix.size(); i++) {
+            if(!node -> containsKey(prefix[i])) {
+                return false;
+            }
+            node = node -> get(prefix[i]);
+        }
+        return true;
+    }
+};
