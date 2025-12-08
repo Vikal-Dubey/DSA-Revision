c86df3f642dc3bc31f7f51c7c398a43b66103a9a
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-12-08 20:17:15 +0530

    Added < 98. Validate Binary Search Tree >

---
+++ Binary Tree/isValidBST.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    
+    class Solution {
+    public:
+        long long maxTree(TreeNode* root){
+            if(root == NULL)    return LLONG_MIN;
+            return max((long long)root->val,max(maxTree(root->left),maxTree(root->right)));
+        }
+        long long minTree(TreeNode* root){
+            if(root == NULL)    return LLONG_MAX;
+            return min((long long)root->val,min(minTree(root->left),minTree(root->right)));
+        }
+        bool isValidBST(TreeNode* root) {
+            if(root == NULL)    return true;
+            else if((long long)(root->val) <= maxTree(root->left))    return false;
+            else if((long long)(root->val) >= minTree(root->right))    return false;
+            return isValidBST(root->left) && isValidBST(root->right);
+        }
+    };
+    return 0;
+}
