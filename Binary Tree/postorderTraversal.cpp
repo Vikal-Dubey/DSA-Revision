c7449e146edcb457ac803acc8c87bb76133298f1
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-12-08 19:46:07 +0530

    Added <145. Binary Tree Postorder Traversal >

---
+++ Binary Tree/postorderTraversal.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+    
+    class Solution {
+    public:
+        void postOrder(TreeNode* root,vector<int>& ans){
+            if(root == NULL)    return;
+            postOrder(root->left,ans);      
+            postOrder(root->right,ans);
+            ans.push_back(root->val);
+        }
+        vector<int> postorderTraversal(TreeNode* root) {
+            vector<int> ans;
+            postOrder(root,ans);
+            return ans;
+        }
+    };
+
+    return 0;
+}
