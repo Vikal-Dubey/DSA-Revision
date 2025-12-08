c23515bad3646c3f0f32d58f2a53bf1798487271
Vikal-Dubey <vikaldubey2004@gmail.com>
2025-12-08 15:49:56 +0530

    Added < 83. Remove Duplicates from Sorted List >

---
+++ Linked List/deleteDuplicates.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    class Solution {
+    public:
+        ListNode* deleteDuplicates(ListNode* head) {
+            if(head == NULL || head->next == NULL)  return head;
+            ListNode* a = head;
+            ListNode* b = head->next;
+            while(b!=NULL){
+                while(b!=NULL && b->val==a->val){
+                    b = b->next;
+                }
+                a->next = b;
+                a = b;
+                if(b!= NULL) b = b->next;
+            }
+            return head;
+        }
+    };
+    return 0;
+}
